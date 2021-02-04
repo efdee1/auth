7cafee6c687d6ef5c62fad54c23a146db9545f3e
efdee1 <feranmi.adejinmi@gmail.com>
2021-02-04 21:21:07 +0100

    First commit,adding a splash screen

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mufasa Auth',
+      home: Splash(),
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 3,
+      backgroundColor: Colors.amber,
+      image: Image.asset('assets/launch_image.png'),
+      loaderColor: Colors.white,
+      photoSize: 100,
+      navigateAfterSeconds: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(child: Text('LoginScreen',
+          style: TextStyle(
+            color: Colors.black
+          ),
+        ),
+
+        ),
+      ),
+    );
+  }
+}
+
+
